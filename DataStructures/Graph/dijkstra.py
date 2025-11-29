5fed421a712305dddfd05ad11262ae17f88f876c
Mateo-Avila <m.avilab2@uniandes.edu.co>
2025-11-28 23:57:05 -0500

    4

--- DataStructures/Graph/dijkstra.py
+++ DataStructures/Graph/dijkstra.py
@@ -1,11 +1,14 @@
 from DataStructures.Map import map_linear_probing as mlp
 from DataStructures.Graph import diagraph as G
 from DataStructures.Queue import queue as pq
-from DataStructures.Graph import dijsktra_structure
+from DataStructures.Graph import dijsktra_structure 
+from DataStructures.Stack import stack as st
+from DataStructures.Graph import vertex as v
+
 import math
 
 def init_structure(graph, source):
-    structure = dijkstra_structure.new_dijkstra_structure(source, G.order(graph))
+    structure = dijsktra_structure.new_dijsktra_structure(source, G.order(graph))
     vertices = G.vertices(graph)
     for i in range(lt.size(vertices)):
         vert = lt.get_element(vertices, i)
@@ -23,3 +26,24 @@ def has_path_to(vertex, structure):
             return True
 
     return False
+
+def path_to(visited,key_v):
+
+    info = map.get(visited, key_v)
+    if info is None:
+        return None
+
+    info = info["value"]
+
+    if info["dist_to"] == math.inf:
+        return None
+
+    stack = st.new_stack()
+
+    v = key_v
+    while v is not None:
+        st.push(stack, v)
+        v = mlp.get(visited, v)["value"]["edge_from"]
+
+    return stack
+
